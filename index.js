7ce49366112f345cc065b74013eced0db451d229
Miracle8221 <anagwumiracle098@gmail.com>
2023-09-12 19:32:51 +0100

    Add game to play

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ function getComputerChoice(){
     console.log("Computer picked" + " " + choices[choice])
     return choices[choice]
 }
+// to create round for the playerSelection and computerSelection
 function round(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     if(!["rock", "paper", "scissors"].includes(playerSelection)) {
         return "invalid input.Please choose rock, paper, scissors.";
     }
-    
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+      }
+      if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+      ) {
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+      } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+      }
 }
+// to create game for playerSelection and computerSelection
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+}  
